5e92e764af833454bd08b908d5d787337b21dda2
Diego <diegoabustos24@gmail.com>
2022-05-05 13:16:37 -0500

    upload project

---
+++ src/components/LaunchList/index.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { useLaunchListQuery } from '../../generated/graphql';
+import LaunchList from './LaunchList';
+import { OwnProps } from './LaunchList';
+
+
+
+const LaunchListContainer = ({handleIdChange}: OwnProps) => {
+  const { data, error, loading } = useLaunchListQuery();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error || !data) {
+    return <div>ERROR</div>;
+  }
+
+  return <LaunchList data={data} handleIdChange={handleIdChange}/>;
+};
+
+export default LaunchListContainer;
